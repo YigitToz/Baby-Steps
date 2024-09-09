7905c98567f7ca006dad1543796e4a9fa1d57542
Yiğit Efe Toz <yigitefetoz@gmail.com>
2024-09-09 21:04:51 +0200

    Add files via upload

---
+++ Fonksiyonlar.py
@@ -0,0 +1,17 @@
+print("a", "b")
+print("a", "b", sep="__")
+help(print)
+#FONKSIYON TANIMLAMA
+
+
+def calculate(x):
+    print(x*2)
+
+calculate(5)
+
+# 2 Argümanlı ve 2 parametreli bir fonksiyon tanımlama
+
+def summer(arg1, arg2):
+    print(arg1 + arg2)
+
+summer(7, 8)
