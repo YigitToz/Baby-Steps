2361e669d0e376171dc13b56bd5cb6196800d144
Yiğit Efe Toz <yigitefetoz@gmail.com>
2024-09-11 23:40:04 +0300

    Add files via upload

---
+++ docstring.py
@@ -0,0 +1,23 @@
+#Docstring / Google
+
+def summer(arg1, arg2):
+    print(arg1+arg2)
+
+def summer(arg1, arg2):
+    """
+Sum of two numbers
+
+    Args:
+        arg1: int, float
+        arg2: int, float
+
+    Returns:
+int, float
+
+Examples:
+Notes:
+
+    """
+    print(arg1+arg2)
+
+summer(1, 3)
